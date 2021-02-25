859453f89f49ef52c491d94b58dbfbc1bd223c4e
Daniel-dadu <dadu9494@gmail.com>
2021-02-25 17:34:23 -0600

    improve suma with more code #1

--- suma.cpp
+++ suma.cpp
@@ -6,6 +6,7 @@ int main(){
 	int n, tmp, res = 0;
 	cout << "Cuantos números vas a sumar: ";
 	cin >> n;
+	cout << "RECUERDA, SOLO NÚMEROS PEQUEÑOS xd" << endl;
 	for(int i = 0; i < n; i++){
 		cout << "Dame el número " << i+1 << ": ";
 	       	cin >> tmp;
